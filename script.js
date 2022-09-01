a82ec6817ed368660cc6f8a207d0436fc781497d
maemeier <maemeier@live.com>
2022-09-01 17:36:08 +0200

    fix games

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ function submit_button(){
   document.getElementById('answer').value ="Yippy! The answer is correct"
   event.target.innerHTML = "Next question"
 }
+
+
+
+function showQuestion(q){
+  let titleDev = document.getElementById("title");
+  titleDev.textContent = q.title
+}
+let questions = {
+  title : "🏗️ 📸",
+  correctAnswer: "lion king"
+}
+
+
+showQuestion(questions)

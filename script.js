d8330aeebe8da8a56d81a1441fbafab2a7db3038
maemeier <maemeier@live.com>
2022-09-04 01:36:05 +0200

    change focus

--- script.js
+++ script.js
@@ -4,18 +4,15 @@ function submit_button(element){
 
 let correct = document.getElementById('answer').value
 if(correct == "sagrada familia"){
-  document.getElementById('answer').value = "correct answer!!"
+  document.getElementById('answer').value = "Yippy, correct answer!!"
   document.getElementById('answer').style.color = "#078469"
 } else {
-  document.getElementById('answer').value = "wrong answer 😢"
+  document.getElementById('answer').value = "Opps, wrong answer 😢"
   document.getElementById('answer').style.color = "#9D0000"
 }
 event.target.innerHTML = "Next question"
 
-
 }
-
-
 
 
 function showQuestion(q){

ca7f9590fc99a1ed12e69173a0b3834a96cfb293
maemeier <maemeier@live.com>
2022-09-05 12:14:18 +0200

    set time

---
+++ quiz.js
@@ -0,0 +1,23 @@
+const quiz = [
+  {
+    question:"🏗️ 📸",
+    answer: "sagrada familia"
+  },
+  {
+    question:"🐇 🎩 🌿",
+    answer: "alice garden"
+  },
+  {
+    question:"⛲",
+    answer: "alice garden"
+  },
+  {
+    question:"🍎 🛍️",
+    answer: "apple store"
+  },
+  {
+    question:"🏟️ ⛺",
+    answer: "camp nou"
+  }
+
+]
--- script.js
+++ script.js
@@ -1,27 +1,39 @@
-function submit_button(element){
+let n=0;
 
+function submit_button(element) {
+  let correct = document.getElementById("answer").value.toLowerCase();
+  if (correct == "sagrada familia") {
+    document.getElementById("answer").value = "Yippy, correct answer!!";
+    document.getElementById("answer").style.color = "#078469";
+    n += 1;
+    setTimeout(()=>{showQuestion(questions[n])}, 1000);
+  } else {
+    document.getElementById("answer").value = "Opps, wrong answer 😢";
+    document.getElementById("answer").style.color = "#9D0000";
+    n += 1;
+    setTimeout(()=>{showQuestion(questions[n])}, 1000);
 
-
-let correct = document.getElementById('answer').value
-if(correct == "sagrada familia"){
-  document.getElementById('answer').value = "Yippy, correct answer!!"
-  document.getElementById('answer').style.color = "#078469"
-} else {
-  document.getElementById('answer').value = "Opps, wrong answer 😢"
-  document.getElementById('answer').style.color = "#9D0000"
+  }
+  event.target.innerHTML = "Next question";
 }
-event.target.innerHTML = "Next question"
 
-}
-
-
-function showQuestion(q){
+function showQuestion(q) {
   let titleDev = document.getElementById("title");
-  titleDev.textContent = q.title
+  titleDev.textContent = q.title;
 }
-let questions = {
-  title : "🏗️ 📸",
-  correctAnswer: "sagrada familia"
-}
+let questions = [
+  {
+    title: "🏗️ 📸",
+    correctAnswer: "sagrada familia",
+  },
+  {
+    title: "🐇 🎩 🌿",
+    correctAnswer: "alice garden",
+  },
+  {
+    title: "🍎 🛍️",
+    correctAnswer: "alice garden",
+  },
+];
 
-showQuestion(questions)
+showQuestion(questions[n]);
